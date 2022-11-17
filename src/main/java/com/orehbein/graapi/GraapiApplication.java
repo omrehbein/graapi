eda0b854854ce5f2380bc90bf1842325df2c48fe
osmar.rehbein <osmar.rehbein>
2022-11-16 23:21:08 -0300

    first commit

---
+++ src/main/java/com/orehbein/graapi/GraapiApplication.java
@@ -0,0 +1,13 @@
+package com.orehbein.graapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orehbein/graapi/GraapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orehbein.graapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
